f929f4bf9b38ce6290226f04ab07166201d45c33
Gian <gian282004@gmail.com>
2024-05-25 22:51:24 -0300

    "Creacion contenedora "

---
+++ pagina del vivero/src/Vivero.java
@@ -0,0 +1,15 @@
+import Articulos.Articulo;
+import Clientes.Cliente;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Vivero {
+    HashMap<Integer, Cliente> clientes;
+    HashMap<String, LinkedList<Articulo>> Articulos;
+
+
+
+}
